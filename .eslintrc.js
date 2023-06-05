17e76de598ff3aa3bb7ac296a2caa311b712ab0f
張智翔 <ch.chang.job@gmail.com>
2023-06-05 22:28:18 +0800

    fix: Solve for lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,32 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    createDefaultProgram: true
+  },
   env: {
     browser: true,
     es2021: true
   },
   extends: [
-    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'standard-with-typescript',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   plugins: [
+    '@typescript-eslint',
     'react',
     'deprecation'
   ],
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
   rules: {}
 }
