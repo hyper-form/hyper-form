65c583bcb6caecdd6fa56b2d9b321868aabc625a
張智翔 <ch.chang.job@gmail.com>
2023-06-09 21:26:32 +0800

    feat(core): Use with devtools middleware

--- packages/core/src/stores/index.ts
+++ packages/core/src/stores/index.ts
@@ -1,6 +1,7 @@
 import type { State } from './share'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
+import { devtools } from 'zustand/middleware/devtools'
 import { createForm, removeForm } from './form'
 import { setValue, setRemaining, setVisibility } from './field'
 
@@ -13,12 +14,14 @@ interface Actions {
 }
 
 export const useForm = create(
-  immer<State & Actions>((set) => ({
-    forms: {},
-    createForm: createForm(set),
-    removeForm: removeForm(set),
-    setValue: setValue(set),
-    setVisibility: setVisibility(set),
-    setRemaining: setRemaining(set)
-  }))
+  devtools(
+    immer<State & Actions>((set) => ({
+      forms: {},
+      createForm: createForm(set),
+      removeForm: removeForm(set),
+      setValue: setValue(set),
+      setVisibility: setVisibility(set),
+      setRemaining: setRemaining(set)
+    }))
+  )
 )
