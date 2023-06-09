66c5182f9ec8ef6329e8be79b3e3eb5f43380c68
張智翔 <ch.chang.job@gmail.com>
2023-06-09 19:00:15 +0800

    feat(core): Add form error

---
+++ packages/core/src/errors/form-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from 'ts-custom-error'
+
+export enum FormErrorCode {
+  FORM_ALREADY_EXISTS
+}
+
+export class FormError extends CustomError {
+  public constructor(
+    public code: FormErrorCode,
+    message?: string
+  ) {
+    super(message)
+  }
+}
