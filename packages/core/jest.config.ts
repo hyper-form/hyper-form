d3a15e8727ada118b7f9179638930d129a8593b5
張智翔 <ch.chang.job@gmail.com>
2023-06-09 22:45:55 +0800

    feat(core): Use jest with babel

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,8 +1,13 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)']
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
+  testMatch: ['<rootDir>/src/__test__/**/*.(spec|test).ts?(x)'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  verbose: true
 }
 
 export default config
