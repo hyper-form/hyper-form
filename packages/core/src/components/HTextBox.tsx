853dc555d120c4c1947bfac98fd356b1ed1116a1
張智翔 <ch.chang.job@gmail.com>
2023-07-01 22:41:41 +0800

    feat(core): apply changer for textbox

--- packages/core/src/components/HTextBox.tsx
+++ packages/core/src/components/HTextBox.tsx
@@ -1,32 +1,56 @@
-import type { FunctionComponent, InputHTMLAttributes } from 'react'
+import type {
+  FunctionComponent,
+  InputHTMLAttributes,
+  FocusEvent,
+  ChangeEvent
+} from 'react'
 import type { TextBoxType } from '../ui/TextBox'
 import { useInjection } from 'inversify-react'
 import { FieldNameContextProvider } from '../contexts/FieldNameContext'
 import { TextBoxSymbol } from '../ui/TextBox'
 import { Visibility } from '../share/constants'
 import { useHTextBox } from '../hooks/h-textbox'
-import { useFormName } from '../hooks/form-name'
-import { useFieldName } from '../hooks/field-name'
-import { omit } from 'lodash-es'
 import React from 'react'
 
 export type HTextBoxProps = InputHTMLAttributes<HTMLInputElement>
 
 export const HTextBox: FunctionComponent<HTextBoxProps> = (props) => {
-  const formName = useFormName()
-  const fieldName = useFieldName()
-
-  const state = useHTextBox(formName, fieldName)
+  const { onBlur, onFocus, onChange } = props
+  const { fieldName, value, visibility, disabled, changeValue, changeEditing } =
+    useHTextBox()
 
   const TextBox = useInjection<TextBoxType>(TextBoxSymbol)
 
+  const handleFocus = (e: FocusEvent<HTMLInputElement, Element>): void => {
+    changeEditing(fieldName, false)
+    onFocus?.(e)
+  }
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement, Element>): void => {
+    changeEditing(fieldName, true)
+    onBlur?.(e)
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value
+    changeValue(fieldName, value)
+    onChange?.(e)
+  }
+
   return (
     <>
-      {state.visibility !== Visibility.Visible
+      {visibility !== Visibility.Visible
         ? null
         : (
           <FieldNameContextProvider value={fieldName}>
-            <TextBox {...{ ...omit(state, ['visibility']), ...props }} />
+            <TextBox
+              {...props}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              disabled={disabled}
+              value={value}
+            />
           </FieldNameContextProvider>
           )}
     </>
--- packages/core/src/hooks/h-textbox.ts
+++ packages/core/src/hooks/h-textbox.ts
@@ -1,20 +1,39 @@
-import type { InputHTMLAttributes } from 'react'
 import type { Visibility } from '../share/constants'
+import { useEffect } from 'react'
+import { createField } from '../changers/field'
+import { changeValue } from '../changers/value'
+import { changeEditing } from '../changers/editing'
 import { useFieldVisibility } from './field-visibility'
 import { useFieldDisabled } from './field-disabled'
 import { useFieldValue } from './field-value'
+import { useFormName } from './form-name'
+import { useFieldName } from './field-name'
 
-export type HTextBoxState = {
+export interface HTextBoxState {
   visibility: Visibility
-} & InputHTMLAttributes<HTMLInputElement>
-
-export const useHTextBox = (
-  formName: string,
   fieldName: string
-): HTextBoxState => {
+  formName: string
+  disabled: boolean
+  value: string
+  changeValue: typeof changeValue
+  changeEditing: typeof changeEditing
+}
+
+export const useHTextBox = (): HTextBoxState => {
+  const formName = useFormName()
+  const fieldName = useFieldName()
+
+  useEffect(() => {
+    createField(fieldName)
+  }, [])
+
   return {
+    formName,
+    fieldName,
     visibility: useFieldVisibility(formName, fieldName),
     disabled: useFieldDisabled(formName, fieldName),
-    value: useFieldValue(formName, fieldName)
+    value: useFieldValue(formName, fieldName),
+    changeValue,
+    changeEditing
   }
 }
