14cf294437aaa6438f104b2ce252a7c015c947d3
張智翔 <ch.chang.job@gmail.com>
2023-07-15 14:54:46 +0800

    test(core): add field disabled hook test

---
+++ packages/core/src/__test__/hooks/field-disabled.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import type { ReactElement, ReactNode } from 'react'
+import type { FormStore } from '../../stores'
+import { renderHook } from '@testing-library/react'
+import { Container } from 'inversify'
+import { HFormProvider } from '../../components/HFormProvider'
+import { HForm } from '../../components/HForm'
+import { HBind } from '../../components/HBind'
+import { FormStoreSymbol } from '../../stores'
+import { formStore } from '../mocks/stores/form'
+import { useFieldDisabled } from '../../hooks/field-disabled'
+
+describe('測試欄位禁用狀態鉤子', (): void => {
+  it('測試欄位禁用鉤子取得禁用狀態', (): void => {
+    expect.assertions(1)
+
+    const formName = 'FormName'
+    const fieldName = 'FieldName'
+
+    const wrapper = ({ children }: { children: ReactNode }): ReactElement => {
+      const container = new Container()
+      container.bind<FormStore>(FormStoreSymbol).toConstantValue(formStore)
+      return (
+        <HFormProvider container={container}>
+          <HForm formName={formName}>
+            <HBind fieldName={fieldName}>{children}</HBind>
+          </HForm>
+        </HFormProvider>
+      )
+    }
+
+    const { result } = renderHook(() => useFieldDisabled(formName, fieldName), {
+      wrapper
+    })
+    const { current } = result
+
+    expect(current).toBe(false)
+  })
+})
