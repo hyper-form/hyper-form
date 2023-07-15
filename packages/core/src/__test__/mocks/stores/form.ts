2bbce56b372d33e43a933489e7db2c9a65466508
張智翔 <ch.chang.job@gmail.com>
2023-07-15 14:53:53 +0800

    test(core): adjust for mock store

--- packages/core/src/__test__/mocks/stores/form.ts
+++ packages/core/src/__test__/mocks/stores/form.ts
@@ -1,9 +1,26 @@
 import type { FormStore } from '../../../stores/form'
-import { noop } from 'lodash-es'
+import { noop, constant } from 'lodash-es'
+import { Visibility } from '../../../share/constants'
 
 export const formStore: FormStore = {
-  useForm: () => ({
-    createForm: noop,
-    removeForm: noop
-  })
+  createForm: noop,
+  removeForm: noop,
+  createField: noop,
+  removeField: noop,
+  createArrayField: noop,
+  removeArrayField: noop,
+  createArrayFieldItem: noop,
+  removeArrayFieldItem: noop,
+  changeValue: noop,
+  changeVisibility: noop,
+  changeAlive: noop,
+  changeEditing: noop,
+  changeDirty: noop,
+  changeDisabled: noop,
+  getValue: constant(''),
+  getVisibility: (formName: string, fieldName: string) => Visibility.Visible,
+  getAlive: constant(true),
+  getEditing: constant(true),
+  getDirty: constant(true),
+  getDisabled: constant(false)
 }
