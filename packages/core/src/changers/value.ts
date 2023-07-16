831f4d9f5209794692420d2acb80f170f6807163
張智翔 <ch.chang.job@gmail.com>
2023-07-16 22:22:09 +0800

    feat(core): solve for basic structure for value changer

--- packages/core/src/changers/value.ts
+++ packages/core/src/changers/value.ts
@@ -1,7 +1,20 @@
+import type { FormStore } from '../stores/form'
+import { FormStoreSymbol } from '../stores/form'
+import { getContainer } from '../share/container'
+
 export const changeValue = (
   formName: string,
   fieldName: string,
   value: string
 ): void => {
-  console.log('')
+  const container = getContainer()
+  const store = container.get<FormStore>(FormStoreSymbol)
+
+  // TODO: 發出事件 - before change
+  store.changeValue(formName, fieldName, value)
+  // TODO: 發出事件 - after change
+
+  // TODO: 執行資料處理器
+  // TODO: 執行可見處理器
+  // TODO: 執行禁用處理器
 }
