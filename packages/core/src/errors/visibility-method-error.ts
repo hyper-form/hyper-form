83e9a186eec85416d04fcba2b35725f7cb79a985
張智翔 <ch.chang.job@gmail.com>
2023-09-25 11:18:13 +0800

    feat(core): add default error message for visibility method error

--- packages/core/src/errors/visibility-method-error.ts
+++ packages/core/src/errors/visibility-method-error.ts
@@ -1,11 +1,25 @@
 import { CustomError } from 'ts-custom-error'
+import { isUndefined } from 'lodash-es'
 
 export enum VisibilityMethodErrorCode {
   SPECIFIC_METHOD_ID_NOT_EXISTS
 }
 
+const VisibilityMethodErrorMessage: Record<VisibilityMethodErrorCode, string> =
+  {
+    [VisibilityMethodErrorCode.SPECIFIC_METHOD_ID_NOT_EXISTS]:
+      'Specific visibility method id have not been registered'
+  }
+
 export class VisibilityMethodError extends CustomError {
-  public constructor (public code: VisibilityMethodErrorCode, message?: string) {
-    super(message)
+  public constructor (
+    public code: VisibilityMethodErrorCode,
+    customMessage?: string
+  ) {
+    super(
+      isUndefined(customMessage)
+        ? VisibilityMethodErrorMessage[code]
+        : `Validate method error with custom error message: "${customMessage}"`
+    )
   }
 }
