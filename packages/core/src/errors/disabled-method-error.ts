8d127941c4fef7fdbfff304312052a3bd12956bd
張智翔 <ch.chang.job@gmail.com>
2023-09-25 11:35:22 +0800

    feat(core): add disabled method error

---
+++ packages/core/src/errors/disabled-method-error.ts
@@ -0,0 +1,24 @@
+import { CustomError } from 'ts-custom-error'
+import { isUndefined } from 'lodash-es'
+
+export enum DisabledMethodErrorCode {
+  SPECIFIC_METHOD_ID_NOT_EXISTS
+}
+
+const DisabledMethodErrorMessage: Record<DisabledMethodErrorCode, string> = {
+  [DisabledMethodErrorCode.SPECIFIC_METHOD_ID_NOT_EXISTS]:
+    'Specific validate method id have not been registered'
+}
+
+export class ValidateMethodError extends CustomError {
+  public constructor (
+    public code: DisabledMethodErrorCode,
+    customMessage?: string
+  ) {
+    super(
+      isUndefined(customMessage)
+        ? DisabledMethodErrorMessage[code]
+        : `Disabled method error with custom error message: "${customMessage}"`
+    )
+  }
+}
