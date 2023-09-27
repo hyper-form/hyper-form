f3053b718b897b770e06fe00804d94e0495ead11
張智翔 <ch.chang.job@gmail.com>
2023-09-27 11:52:55 +0800

    feat(core): add disabled rule error

---
+++ packages/core/src/errors/disabled-rule-error.ts
@@ -0,0 +1,24 @@
+import { CustomError } from 'ts-custom-error'
+import { isUndefined } from 'lodash-es'
+
+export enum DisabledRuleErrorCode {
+  SPECIFIC_RULE_ID_NOT_EXISTS
+}
+
+const DisabledRuleErrorMessage: Record<DisabledRuleErrorCode, string> = {
+  [DisabledRuleErrorCode.SPECIFIC_RULE_ID_NOT_EXISTS]:
+    'Specific disabled rule id have not been added'
+}
+
+export class DisabledRuleError extends CustomError {
+  public constructor (
+    public code: DisabledRuleErrorCode,
+    customMessage?: string
+  ) {
+    super(
+      isUndefined(customMessage)
+        ? DisabledRuleErrorMessage[code]
+        : `Disabled rule error with custom error message: "${customMessage}"`
+    )
+  }
+}
