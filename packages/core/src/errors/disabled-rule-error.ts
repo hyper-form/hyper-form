d3631189598b029cf3047d608f7885ca941d77b8
張智翔 <ch.chang.job@gmail.com>
2023-09-27 16:40:47 +0800

    feat(core): add new disabled rule error code

--- packages/core/src/errors/disabled-rule-error.ts
+++ packages/core/src/errors/disabled-rule-error.ts
@@ -2,10 +2,13 @@ import { CustomError } from 'ts-custom-error'
 import { isUndefined } from 'lodash-es'
 
 export enum DisabledRuleErrorCode {
+  SPECIFIC_FORM_NOT_EXISTS,
   SPECIFIC_RULE_ID_NOT_EXISTS
 }
 
 const DisabledRuleErrorMessage: Record<DisabledRuleErrorCode, string> = {
+  [DisabledRuleErrorCode.SPECIFIC_FORM_NOT_EXISTS]:
+    'Specific form have not been created',
   [DisabledRuleErrorCode.SPECIFIC_RULE_ID_NOT_EXISTS]:
     'Specific disabled rule id have not been added'
 }
